4fcd77a2c86e5aa1085b09c30377753b09cbbe53
Caio-Higa <caio.hhiga@gmail.com>
2026-08-21 18:44:08 -0300

    Add files via upload

---
+++ aula1908pa.py
@@ -0,0 +1,9 @@
+number1 = 20;
+number2 = 40;
+
+somaResult = number1 + number2;
+subResult = number1 - number2;
+multResult = number1 * number2;
+divResult = number1 / number2;
+
+print (somaResult)
---
+++ testeBhaskara2108pa.py
@@ -0,0 +1,15 @@
+numA = 1;
+numB = -5;
+numC = 6;
+
+delta = (numB * numB) - (4 * numA * numC);
+
+print ("Delta = " , delta);
+
+bhaskRaiz1 = (-numB + delta ** 0.5) / (2 * numA)
+
+print ("Raiz 1 = " , bhaskRaiz1);
+
+bhaskRaiz2 = (-numB - delta ** 0.5) / (2 * numA)
+
+print ("Raiz 2 = " , bhaskRaiz2);
